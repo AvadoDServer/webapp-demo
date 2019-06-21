a29ba1244f4d309a14a88cce66672330436ada84
Stefaan Ponnet <stefaan@ponnet.com>
2019-06-21 12:05:57 +0200

    added provider enpoint

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import  { providers } from "ethers";
+import { providers } from "ethers";
 
 function App() {
 
-    const [blockNumber,setBlocknumber] = React.useState();
+    const [blockNumber, setBlocknumber] = React.useState();
 
     React.useEffect(() => {
         const provider = new providers.JsonRpcProvider("http://my.ethchain.dnp.dappnode.eth:8545");
+
         provider.getBlockNumber().then((blockNumber) => {
             setBlocknumber(blockNumber);
         });
+
         provider.on('block', (blockNumber) => {
             setBlocknumber(blockNumber);
         });
 
-    },[]);
+    }, []);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {blockNumber}
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    {blockNumber}
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
